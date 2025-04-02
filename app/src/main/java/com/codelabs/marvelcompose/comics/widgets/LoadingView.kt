dc6960e2a0408505ac924ee66fdd576ec4ddb2f0
RuiAlonso <rui.alonso@verygood.ventures>
2025-04-02 18:35:03 +0200

    feat: loading animation

--- app/src/main/java/com/codelabs/marvelcompose/comics/widgets/LoadingView.kt
+++ app/src/main/java/com/codelabs/marvelcompose/comics/widgets/LoadingView.kt
@@ -1,15 +1,57 @@
 package com.codelabs.marvelcompose.comics.widgets
 
+import android.view.animation.LinearInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.codelabs.marvelcompose.R
+import kotlinx.coroutines.launch
 
 @Composable
-fun LoadingView() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "Loading...")
+fun LoadingView(height: Float? = 300f) {
+    val rotation = remember { Animatable(0f) }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(Unit) {
+        coroutineScope.launch {
+            while (true) {
+                rotation.animateTo(
+                    targetValue = 360f,
+                    animationSpec = tween(
+                        durationMillis = 1000,
+                        easing = LinearInterpolator()::getInterpolation
+                    )
+                )
+                rotation.snapTo(0f)
+            }
+        }
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .padding(20.dp)
+            .fillMaxSize()
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(R.drawable.mjolnir),
+            contentDescription = null,
+            modifier = Modifier
+                .rotate(rotation.value)
+                .height(height?.dp ?: 300.dp)
+        )
     }
 }
